a0b51362fdf14f4eb3a2f6706edb9b48ebb36f5e
Ayaulym <180103201@stu.sdu.edu.kz>
2022-04-20 15:23:06 +0600

    renamed main

--- main-service/src/main/java/com/bookross/mainservice/demo/BookrossApplication.java
+++ main-service/src/main/java/com/bookross/mainservice/demo/BookrossApplication.java
@@ -2,13 +2,12 @@ package com.bookross.mainservice.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication()
-public class DemoApplication {
+public class BookrossApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(BookrossApplication.class, args);
     }
 
 }
