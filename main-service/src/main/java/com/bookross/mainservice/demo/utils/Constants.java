1cdeca76fe324067aaac64af2319844765b2edd1
Ayaulym <180103201@stu.sdu.edu.kz>
2022-04-20 15:24:35 +0600

    added class for constants

---
+++ main-service/src/main/java/com/bookross/mainservice/demo/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.bookross.mainservice.demo.utils;
+
+public final class Constants {
+    private Constants(){}
+    public static final String PHONE_REGEX = "[0-9]{11}";
+    public static final int PHONE_LENGTH = 11;
+}
