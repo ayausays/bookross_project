0ea448c1463504f5f9921824f29235f6c85940da
Shakhmardan <190103454@gmail.com>
2022-04-29 01:01:03 +0600

    added missing ChatController.java

--- main-service/src/main/java/com/bookross/mainservice/demo/controller/ChatController.java
+++ main-service/src/main/java/com/bookross/mainservice/demo/controller/ChatController.java
@@ -1,22 +1,24 @@
 package com.bookross.mainservice.demo.controller;
 
-import com.bookross.mainservice.demo.entity.Message;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
 
-@Controller
-public class ChatController {
+import com.bookross.mainservice.demo.entity.request.MessagePostResource;
+import com.bookross.mainservice.demo.entity.response.GetMessageResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.*;
 
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
+@RequestMapping("/api/v1/message")
+@CrossOrigin("*")
+public interface ChatController {
 
-    @MessageMapping("private-message")
-    public Message receivePrivateMessage(@Payload Message message) {
-        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private",message); // /user/Shakh/private
-        return message;
-    }
+    @PostMapping("/send/{id}/{me}")
+    public ResponseEntity<GetMessageResponse> sendMessage(@PathVariable("id") Long receiverId,
+                                                          @PathVariable("me") Long me,
+                                                          @RequestBody MessagePostResource messagePostResource
+    );
+
+    @GetMapping("/get/{userId}/{page}/{me}")
+    public ResponseEntity<Page<GetMessageResponse>> getAUserMessages(@PathVariable String userId, @PathVariable int page,@PathVariable Long me);
 
 }
