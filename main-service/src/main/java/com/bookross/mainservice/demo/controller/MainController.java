78a4bd23a7404708648e77245999d47293305c5d
Ayaulym <180103201@stu.sdu.edu.kz>
2022-05-06 10:40:01 +0600

    main, image

--- main-service/src/main/java/com/bookross/mainservice/demo/controller/MainController.java
+++ main-service/src/main/java/com/bookross/mainservice/demo/controller/MainController.java
@@ -9,22 +9,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(path = "api/v1/registration")
 @RequiredArgsConstructor
 public class MainController {
+
     private final RegistrationService registrationService;
     private final LoginService loginService;
 
-    @PostMapping(path = "register")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping(path = "register", consumes = {"application/json"})
+    // works postman
     public String register(@RequestBody RegistrationRequest registrationRequest){
         return registrationService.register(registrationRequest);
     }
+
     @PostMapping(path = "login")
+    // works postman
     public String login(@RequestBody LoginRequest loginRequest){
         return loginService.login(loginRequest);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping(path = "confirm")
+    // works postman
     public String confirm(@RequestParam("token") String token){
         return registrationService.confirmToken(token);
     }
